412480fbd359aac677380adf1f303378e5439528
mmatteo-hub <S4636216@studenti.unige.it>
2022-10-18 14:53:33 +0000

    DISJOINT command still to be fixed

--- scripts/init_state.py
+++ scripts/init_state.py
@@ -17,9 +17,25 @@ client.utils.load_ref_from_file(path + "topological_map.owl", "http://bnc/exp-ro
 client.utils.mount_on_ref()
 client.utils.set_log_to_terminal(True)
 
-client.manipulation.add_ind_to_class("C1", "Room")
+client.manipulation.add_objectprop_to_ind("hasDoor", 'E', 'D5')
+client.manipulation.add_objectprop_to_ind("hasDoor", 'E', 'D6')
+client.manipulation.add_objectprop_to_ind("hasDoor", 'C1', 'D1')
+client.manipulation.add_objectprop_to_ind("hasDoor", 'C1', 'D2')
+client.manipulation.add_objectprop_to_ind("hasDoor", 'C1', 'D5')
+client.manipulation.add_objectprop_to_ind("hasDoor", 'C1', 'D7')
+client.manipulation.add_objectprop_to_ind("hasDoor", 'C2', 'D3')
+client.manipulation.add_objectprop_to_ind("hasDoor", 'C2', 'D4')
+client.manipulation.add_objectprop_to_ind("hasDoor", 'C2', 'D5')
+client.manipulation.add_objectprop_to_ind("hasDoor", 'C2', 'D6')
+client.manipulation.add_objectprop_to_ind("hasDoor", 'R1', 'D1')
+client.manipulation.add_objectprop_to_ind("hasDoor", 'R2', 'D2')
+client.manipulation.add_objectprop_to_ind("hasDoor", 'R3', 'D3')
+client.manipulation.add_objectprop_to_ind("hasDoor", 'R4', 'D4')
+client.manipulation.add_objectprop_to_ind("isIn", 'Robot1', 'E')
+
+client.call('DISJOINT', 'IND', '', ['Robot1', 'E', 'C1', 'C2', 'R1', 'R2', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7'])
 
 client.utils.apply_buffered_changes()
 client.utils.sync_buffered_reasoner()
 
-client.utils.save_ref_with_inferences(path + "test.owl")
+client.utils.save_ref_with_inferences(path + "test_topology.owl")
