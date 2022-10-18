29c7af303bde9e3939192c246d822a16c217a091
mmatteo-hub <S4636216@studenti.unige.it>
2022-10-18 17:51:19 +0000

    topology

--- scripts/init_state.py
+++ scripts/init_state.py
@@ -17,23 +17,29 @@ client.utils.load_ref_from_file(path + "topological_map.owl", "http://bnc/exp-ro
 client.utils.mount_on_ref()
 client.utils.set_log_to_terminal(True)
 
+client.manipulation.add_ind_to_class('robot', "Robot")
+
 client.manipulation.add_objectprop_to_ind("hasDoor", 'E', "D5")
 client.manipulation.add_objectprop_to_ind("hasDoor", 'E', "D6")
+
 client.manipulation.add_objectprop_to_ind("hasDoor", 'C1', "D1")
 client.manipulation.add_objectprop_to_ind("hasDoor", 'C1', "D2")
 client.manipulation.add_objectprop_to_ind("hasDoor", 'C1', "D5")
 client.manipulation.add_objectprop_to_ind("hasDoor", 'C1', "D7")
+
 client.manipulation.add_objectprop_to_ind("hasDoor", 'C2', "D3")
 client.manipulation.add_objectprop_to_ind("hasDoor", 'C2', "D4")
 client.manipulation.add_objectprop_to_ind("hasDoor", 'C2', "D5")
 client.manipulation.add_objectprop_to_ind("hasDoor", 'C2', "D6")
+
 client.manipulation.add_objectprop_to_ind("hasDoor", 'R1', "D1")
 client.manipulation.add_objectprop_to_ind("hasDoor", 'R2', "D2")
 client.manipulation.add_objectprop_to_ind("hasDoor", 'R3', "D3")
 client.manipulation.add_objectprop_to_ind("hasDoor", 'R4', "D4")
-#client.manipulation.add_objectprop_to_ind("isIn", 'Robot1', "E")
 
-client.call('DISJOINT', 'IND', '', ['E', 'C1', 'C2', 'R1', 'R2', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7'])
+client.manipulation.add_objectprop_to_ind("isIn", 'robot', "E")
+
+client.call('DISJOINT', 'IND', '', ['E','C1','C2','R1','R2','R3','R4','D1','D2','D3','D4','D5','D6','D7'])
 
 client.manipulation.add_dataprop_to_ind('visitedAt', 'R1', 'Long', '123')
 client.manipulation.add_dataprop_to_ind('visitedAt', 'R2', 'Long', '123')
@@ -43,9 +49,7 @@ client.manipulation.add_dataprop_to_ind('visitedAt', 'C1', 'Long', '123')
 client.manipulation.add_dataprop_to_ind('visitedAt', 'C2', 'Long', '123')
 client.manipulation.add_dataprop_to_ind('visitedAt', 'E', 'Long', '123')
 
-
 client.utils.apply_buffered_changes()
 client.utils.sync_buffered_reasoner()
-client.utils.save(path + "test_topology.owl")
 
-
+client.utils.save(path + "testtop.owl")
