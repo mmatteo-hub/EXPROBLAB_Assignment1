fa2c7be85f569cbdd0c67650e331d68f95400b64
mmatteo-hub <S4636216@studenti.unige.it>
2022-10-20 16:45:08 +0000

    first version of the reasoner

--- scripts/reasoner.py
+++ scripts/reasoner.py
@@ -6,11 +6,15 @@ import rospy
 import actionlib
 import smach
 import smach_ros
+import random
 from EXPROBLAB_Assignment1 import name_mapper as nm
 
 class Reasoner(smach.State):
 	def __init__(self, helper):
 		self._helper = helper
+		
+		self.client = self._helper.client.query
+		
 		# initialisation function, it should not wait
 		smach.State.__init__(self, 
 								outcomes=[nm.BATTERY_OK, nm.BATTERY_LOW, nm.LOADED_ONTOLOGY, nm.REASONED, nm.PLANNED_PATH, nm.LOCATION_REACHED],
@@ -25,7 +29,25 @@ class Reasoner(smach.State):
 		while not rospy.is_shutdown():
 			self._helper.mutex.acquire()
 			try:
-				print("reasoner")
+				if self._helper.action_for_change == nm.BATTERY_LOW:
+					planner_client.cancel_goal()
+					return nm.BATTERY_LOW
+				self._helper.choice = self._check_accessible_location()
+				return nm.REASONED
 			finally:
 				self._helper.mutex.release()
 			rospy.sleep(0.3)
+
+	def _check_accessible_location(self):
+		reachable_locations = self.client.objectprop_b2_ind('canReach','robot')
+		reachable_locations = self._string_adjust(reachable_locations)
+		return self._choose_destination(reachable_locations)
+		
+	def _string_adjust(self, location):
+		for el in range(len(location)):
+			index = location[el].index('#')
+			location[el] = location[el][index+1:-1]
+		return location
+		
+	def _choose_destination(self, locations):
+		return random.choice(locations)
