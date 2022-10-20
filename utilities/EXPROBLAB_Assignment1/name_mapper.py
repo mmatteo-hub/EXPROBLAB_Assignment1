16a0b0ba4b7ef6f5a01f0424d8c2a28d2405a826
mmatteo-hub <S4636216@studenti.unige.it>
2022-10-20 16:42:18 +0000

    modified name mapper

--- utilities/EXPROBLAB_Assignment1/name_mapper.py
+++ utilities/EXPROBLAB_Assignment1/name_mapper.py
@@ -19,6 +19,16 @@ REASONED = 'REASONED'
 PLANNED_PATH = 'PLANNED_PATH'
 LOCATION_REACHED = 'LOCATION_REACHED'
 
+##########
+ACTION_PLANNER = 'motion/planner'
+ACTION_CONTROLLER = 'motion/controller'
+NODE_PLANNER = 'planner'
+NODE_CONTROLLER = 'controller'
+NODE_ROBOT_STATE = 'robot-state'
+
+SERVER_GET_POSE = 'state/get_pose'
+SERVER_SET_POSE = 'state/set_pose'
+
 # Function used to label each log with a producer tag.
 def tag_log(msg, producer_tag):
     return '@%s>> %s' % (producer_tag, msg)
