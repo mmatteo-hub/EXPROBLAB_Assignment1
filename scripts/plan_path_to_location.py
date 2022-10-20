05f3be16062d82029d8689def8a1e0d72441a839
mmatteo-hub <S4636216@studenti.unige.it>
2022-10-20 16:46:14 +0000

    first version of the plan path state

--- scripts/plan_path_to_location.py
+++ scripts/plan_path_to_location.py
@@ -6,11 +6,15 @@ import rospy
 import actionlib
 import smach
 import smach_ros
+import random
+from EXPROBLAB_Assignment1.msg import Point, PlanAction, PlanGoal, ControlAction
+from actionlib_msgs.msg import GoalStatus
 from EXPROBLAB_Assignment1 import name_mapper as nm
 
 class PlanPathToLocation(smach.State):
 	def __init__(self, helper):
 		self._helper = helper
+		self.client = self._helper.client.query
 		# initialisation function, it should not wait
 		smach.State.__init__(self, 
 								outcomes=[nm.BATTERY_OK, nm.BATTERY_LOW, nm.LOADED_ONTOLOGY, nm.REASONED, nm.PLANNED_PATH, nm.LOCATION_REACHED],
@@ -19,13 +23,27 @@ class PlanPathToLocation(smach.State):
 		
 	def execute(self, userdata):
 		# function called when exiting from the node, it can be blacking
-		rospy.loginfo('Executing state ' + nm.PLAN_LOCATION_TO_VISIT + ' (users = %f)'%userdata.plan_path_to_location_counter_in)
+		rospy.loginfo('Executing state ' + nm.PLAN_PATH_TO_LOCATION + ' (users = %f)'%userdata.plan_path_to_location_counter_in)
 		userdata.plan_path_to_location_counter_out = userdata.plan_path_to_location_counter_in + 1
-		
+		self._plan_location()
 		while not rospy.is_shutdown():
 			self._helper.mutex.acquire()
 			try:
-				print("plan_path")
+				if self._helper.action_for_change == nm.BATTERY_LOW: # higher priority
+					self._help.planner_client.cancel_goal()
+					return nm.BATTERY_LOW
+				if self._helper.planner_client.get_state() == GoalStatus.SUCCEEDED:
+					return nm.PLANNED_PATH
 			finally:
 				self._helper.mutex.release()
 			rospy.sleep(0.3)
+	
+	def _plan_location(self):
+		goal = PlanGoal()
+		goal.start = self._start_position()
+		goal.target = Point(x = random.uniform(0, 10), y = random.uniform(0, 10))
+		self._helper.planner_client.send_goal(goal)
+	
+	def _start_position(self):
+		pos = self.client.objectprop_b2_ind('isIn','robot')
+		return Point(x = 1, y = 1)
