e121bceba9d59fa57e8f60439a8c54d7b9b81ae6
mmatteo-hub <S4636216@studenti.unige.it>
2022-10-20 16:44:41 +0000

    added helper field to share among all the classes

--- scripts/helper.py
+++ scripts/helper.py
@@ -7,6 +7,8 @@ import actionlib
 import smach
 import smach_ros
 from EXPROBLAB_Assignment1 import name_mapper as nm
+from EXPROBLAB_Assignment1.msg import Point, PlanAction, PlanGoal, ControlAction
+from armor_api.armor_client import ArmorClient
 
 from threading import Thread
 from threading import Lock
@@ -17,3 +19,9 @@ class Helper:
 			self.mutex = Lock()
 		else:
 			self.mutex = mutex
+		self.action_for_change = ''
+		# client for the arMOR server
+		self.client = ArmorClient("armor_client", "reference")
+		self.planner_client = actionlib.SimpleActionClient('motion/planner', PlanAction)
+		self.controller_client = actionlib.SimpleActionClient('motion/controller', ControlAction)
+	
