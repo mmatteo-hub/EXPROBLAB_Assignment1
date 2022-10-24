85df90b3aaa1cafcee091944e245697c75e13d76
mmatteo-hub <S4636216@studenti.unige.it>
2022-10-24 21:26:43 +0000

    added a name mapper

--- utilities/EXPROBLAB_Assignment1/name_mapper.py
+++ utilities/EXPROBLAB_Assignment1/name_mapper.py
@@ -29,6 +29,16 @@ NODE_ROBOT_STATE = 'robot-state'
 SERVER_GET_POSE = 'state/get_pose'
 SERVER_SET_POSE = 'state/set_pose'
 
+##########
+BATTERY_THRESHOLD = 60
+NUMBER_OF_POINTS_PATH = 10
+RECHARGING_ROOM = 'E'
+
+##########
+# rooms' coordinate
+ROOMS = ['E', 'C1', 'C2', 'R1', 'R2', 'R3', 'R4']
+COORDINATES = [[0.0,0.0], [2.0,0.0], [3.0,0.0], [1.0,0.0], [1.0,1.0], [4.0,0.0], [4.0,1.0]]
+
 # Function used to label each log with a producer tag.
 def tag_log(msg, producer_tag):
     return '@%s>> %s' % (producer_tag, msg)
